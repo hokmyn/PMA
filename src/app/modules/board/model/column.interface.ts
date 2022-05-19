1d33efec2b96a7f1fee61be96bd2915f97e21cf0
hokmyn <den_from_bykhov1988@mail.ru>
2022-05-19 13:29:04 +0300

    feat: add pma project

--- src/app/modules/board/model/column.interface.ts
+++
@@ -1,6 +0,0 @@
-import { ITask } from '@shared/models/board-api-response.model';
-
-export interface ITaskOfColumn {
-    task: ITask;
-    columnId: string;
-}
--- src/app/shared/constants/path.constants.ts
+++
@@ -1,1 +0,0 @@
-export const API_URL = 'https://peaceful-reef-42465.herokuapp.com/';
